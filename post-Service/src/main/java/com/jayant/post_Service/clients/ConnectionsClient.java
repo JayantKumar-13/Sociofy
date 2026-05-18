5e8ab66f21af87e168ef44c6b1aa319d76bd9c22
Jayant Kumar <jayantkumar13ssm@gmail.com>
2026-05-19 00:45:13 +0530

    Added clients in Post Service

---
+++ post-Service/src/main/java/com/jayant/post_Service/clients/ConnectionsClient.java
@@ -0,0 +1,15 @@
+package com.jayant.post_Service.clients;
+
+import com.jayant.post_Service.dto.PersonDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@FeignClient(name = "connections-service", path = "/connections")
+public interface ConnectionsClient {
+
+    @GetMapping("/core/first-degree")
+    List<PersonDto> getFirstConnections();
+
+}
